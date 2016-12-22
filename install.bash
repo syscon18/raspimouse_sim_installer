a24eebde4420f4c4ce0d3ba3d51492ef0c76793c
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-23 00:54:18 +0900

    Remove the loop

--- install.bash
+++ install.bash
@@ -7,14 +7,21 @@ ROS_VER=kinetic
 sudo apt-get install ros-${ROS_VER}-desktop-full
 sudo apt-get install ros-${ROS_VER}-gazebo-ros-control ros-${ROS_VER}-ros-controllers
 
-for repo in raspimouse_ros raspimouse_sim ; do
-	source ~/catkin_ws/devel/setup.bash
-	roscd
-	cd ../src
-	[ -e "${repo}" ] || git clone https://github.com/ryuichiueda/${repo}.git
-	cd ..
-	catkin_make
-done
+source ~/catkin_ws/devel/setup.bash
+
+roscd
+cd ../src
+[ -e "raspimouse_ros" ] || git clone https://github.com/ryuichiueda/raspimouse_ros.git
+cd ..
+source ~/catkin_ws/devel/setup.bash
+catkin_make
+
+roscd
+cd ../src
+[ -e "raspimouse_sim" ] || git clone https://github.com/Tiryoh/raspimouse_sim.git
+cd ..
+source ~/catkin_ws/devel/setup.bash
+catkin_make
 
 source ~/catkin_ws/devel/setup.bash
 
