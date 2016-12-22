3a834bdddbb93a280a749b32b92425a1f3a85496
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-22 23:41:23 +0900

    Add the script

---
+++ install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -exv
+
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=kinetic
+[ "$UBUNTU_VER" = "trusty" ] && ROS_VER=indigo
+
+sudo apt-get install ros-${ROS_VER}-desktop-full
+sudo apt-get install ros-${ROS_VER}-gazebo-ros-control ros-${ROS_VER}-ros-controllers
+
+cd ~/catkin_ws/src/
+git clone https://github.com/ryuichiueda/raspimouse_ros.git
+git clone https://github.com/Tiryoh/raspimouse_sim.git
+
+source /opt/ros/${ROS_VER}/setup.bash
+source ~/catkin_ws/devel/setup.bash
+
+cd ~/catkin_ws
+catkin_make
+
+###HOW TO VERIFY###
+# roslaunch raspimouse_gazebo raspimouse_with_samplemaze.launch 
+# rosrun raspimouse_control controller_vel_publisher.py
